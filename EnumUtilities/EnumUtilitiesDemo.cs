d81ecb74975aa7aad3e8700db4e9fb985f0dba2b
AndersAndreen <anders@re-kreation.se>
2021-04-28 08:45:36 +0200

    Update console demo display layout.

--- EnumUtilities/EnumUtilitiesDemo.cs
+++ EnumUtilities/EnumUtilitiesDemo.cs
@@ -14,25 +14,24 @@ namespace EnumUtilities
 
         public static void RunDemo()
         {
-            Console.WriteLine("## ENUM UTILITIES DEMO ##\n");
+            Console.WriteLine("# ENUM UTILITIES DEMO #\n");
 
-            // Create a list of Enums using an extension method:
+            Console.WriteLine("\n## Create a list of Enums using an extension method:");
             var fruits1 = new Fruits().GetAllEnumValues().PrintList("new Fruits().GetAllEnumValues()");
             var fruits2 = Fruits.Apple.GetAllEnumValues().PrintList("Fruits.apple.GetAllEnumValues()");
 
-            // Create a list of Enums using a static method:
+            Console.WriteLine("\n## Create a list of Enums using a static method:");
             var fruits3 = global::EnumUtilities.EnumUtilities.GetAllEnumValues<Fruits>().PrintList("GetAllEnumValues<Fruits>");
 
-            // Create a list of strings holding the names of all the enums:
+            Console.WriteLine("\n## Create a list of strings holding the names of all the enums:");
             var fruits4 = new Fruits().ToListOfStrings().PrintList("ToListOfStrings");
 
-            // Check if a string has a corresponding Enum:
+            Console.WriteLine("\n## Check if a string has a corresponding Enum:");
             var bool1 = "apple".IsEnum<Fruits>().Print(@"""apple""");
             var bool2 = "banana".IsEnum<Fruits>().Print(@"""banana""");
             var bool3 = "orange".IsEnum<Fruits>().Print(@"""orange""");
-            Console.WriteLine();
 
-            // Convert a string to an Enum specifying a default value if no match is found:
+            Console.WriteLine("\n## Convert a string to an Enum specifying a default value if no match is found:");
             var enum1 = "apple".ToEnum(Fruits.None).Print(@"""apple""");
             var enum2 = "banana".ToEnum(Fruits.None).Print(@"""banana""");
             var enum3 = "orange".ToEnum(Fruits.None).Print(@"""orange""");
@@ -45,9 +44,8 @@ namespace EnumUtilities
     {
         public static IList<T> PrintList<T>(this IList<T> input, string heading)
         {
-            Console.WriteLine($"{heading}:");
+            Console.Write($"{heading}: ");
             Console.WriteLine($"{{ {string.Join(", ", input)} }}");
-            Console.WriteLine();
             return input;
         }
 
